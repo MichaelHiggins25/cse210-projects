17fff4372b37a31a4806027b2034e6abe1c0a114
CodeingWizard24 <hig24010@byui.edu>
2026-01-15 15:01:46 -0700

    new update

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,26 @@
 using System;
 
-class Program
+int number;
+
+Random randomGenerator = new Random();
+int rnumber = randomGenerator.Next(1, 11);
+Console.WriteLine("What is the magic number?");
+do
 {
-    static void Main(string[] args)
+Console.WriteLine("What is your guess?");
+string userInput = Console.ReadLine();
+number = int.Parse(userInput);
+if (number == rnumber)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("You guessed it!");
+    }
+else if (number > rnumber)
+    {
+        Console.WriteLine("Lower");
+    }
+else if (number < rnumber)
+    {
+        Console.WriteLine("Higher");
     }
 }
+while(number != rnumber);
