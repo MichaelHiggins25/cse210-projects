08c103145954daf99e75643b8226d170dc716d54
CodeingWizard24 <hig24010@byui.edu>
2026-01-20 18:43:49 -0700

    Prep 4 changes

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,20 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int usenumber = -1;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (usenumber != 0) 
+        {
+            Console.WriteLine("Enter number:");
+            string userInput = Console.ReadLine();
+            usenumber = int.Parse(userInput);
+            numbers.Add(usenumber);
+        }
+        int sum = numbers.Sum();
+        Console.WriteLine(sum);
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        for (int i = 1; i <= 10; i++ )
+        {
+        Console.WriteLine(i);
+        }
     }
 }
 
