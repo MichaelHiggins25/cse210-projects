f4fab8366933163e37adbec9db2b0ebd8e12f4d0
CodeingWizard24 <hig24010@byui.edu>
2026-01-22 14:51:52 -0700

    sandbox update

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 1; i <= 10; i++ )
-        {
-        Console.WriteLine(i);
-        }
+        
+        
+        
+        
     }
 }
 
