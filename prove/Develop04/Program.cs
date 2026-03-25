a903f55e6d01c88eb2dbc3aaca0f207a1b4d5885
CodeingWizard24 <hig24010@byui.edu>
2026-03-24 21:27:40 -0600

    files made

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -3,19 +3,18 @@ using System;
 class Program
 {
     private static string activity = "";
-    public string dashes = "-------------------------------------------------------------------------";
-    public string arrowndash = $"-------------------------------→ {activity}------------------------------";
+    public static string dashes = "-------------------------------------------------------------------------";
+    public static string arrowndash = $"-------------------------------→ {activity}------------------------------";
     static void Main(string[] args)
     {
         BreathingActivity activity1 = new BreathingActivity("Deep Breaths", "breath in and out deeply until time is up.", 10);
         ListingActivity activity2 = new ListingActivity("List", "",60);
-        ListingActivity activity3 = new ListingActivity("List ", "");
-        ReflectingActivity activity4 = new ReflectingActivity("Reflecting on today","What was the most fun thing you did today?");
-
+        ReflectingActivity activity3 = new ReflectingActivity("Reflecting on today","What was the most fun thing you did today?");
+        Console.WriteLine(dashes);
         Console.WriteLine(activity1);
         Console.WriteLine(activity2);
         Console.WriteLine(activity3);
-        Console.WriteLine(activity4);
+        
     }
 }
 
---
+++ prove/Develop05/Player.cs
@@ -0,0 +1,9 @@
+class Player
+{
+    protected int _totalScore;
+    protected int _level;
+    public void Player()
+    {
+        
+    }
+}
